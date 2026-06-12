99ec57a83e3af623c258ab4b609e9e3776c19c9f
Mithushan-MN <mithushan0099@gmail.com>
2026-06-12 12:48:05 +0530

    multer avif file added

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -99,7 +99,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "ho-assets",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    allowed_formats: ["jpg", "jpeg", "png", "webp", "avif"],
   },
 });
 
